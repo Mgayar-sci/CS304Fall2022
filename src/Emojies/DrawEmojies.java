39e5421825ed68ec3bca9cd9886dc9886d2dbbe5
Mgayar-Sci <dr.mohamedelgayar@gmail.com>
2022-11-22 16:03:36 +0200

    Added 2 listeners for wow ad star emojis

--- src/Emojies/DrawEmojies.java
+++ src/Emojies/DrawEmojies.java
@@ -3,28 +3,40 @@ package Emojies;
 import com.sun.opengl.util.FPSAnimator;
 import java.awt.BorderLayout;
 import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLEventListener;
 import javax.swing.JFrame;
 
 public class DrawEmojies extends JFrame {
 
   private GLCanvas glcanvas;
-    static FPSAnimator animator = null;
-//  private DrawEmojiesGLEventListener1 listener = new DrawEmojiesGLEventListener1();
-  private DrawEmojiesGLEventListener1 listener = new DrawEmojiesGLEventListener1();
+  static FPSAnimator animator = null;
+  static DrawEmojiesGLEventListener wowListener = new DrawEmojiesGLEventListener();
+  static DrawEmojiesGLEventListener1 starListener = new DrawEmojiesGLEventListener1();
 
   public static void main(String[] args) {
-    new DrawEmojies();
-    animator.start();
+    int choice = 0;
+    GLEventListener[] Listeners = { wowListener, starListener };
+    if (args.length > 0) {
+      try {
+        choice = Integer.parseInt(args[0]);
+        new DrawEmojies(Listeners[choice]);
+        animator.start();
+      } catch (ArrayIndexOutOfBoundsException e) {
+        System.err.println("Only 2 listeners are available!");
+      } catch (NumberFormatException e) {
+        System.err.println("Please enter an integer!");
+      }
+    }
   }
 
-  public DrawEmojies() {
+  public DrawEmojies(GLEventListener listener) {
     super("Simple Application");
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     glcanvas = new GLCanvas();
     glcanvas.addGLEventListener(listener);
     animator = new FPSAnimator(glcanvas, 60);
-    
+
     add(glcanvas, BorderLayout.CENTER);
     setSize(800, 800);
     setLocationRelativeTo(this);
